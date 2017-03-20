dbf5ce91c57b18bdca604be22c42854bacf4b0db
loongf <gfjia@msn.com>
2017-03-20 16:05:40 -0400

    with jinja templates

--- headlines.py
+++ headlines.py
@@ -1,5 +1,6 @@
 import feedparser
 from flask import Flask
+from flask import render_template
 
 app=Flask(__name__)
 
@@ -11,24 +12,32 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 
 @app.route('/')
 @app.route('/<publication>')
-def bbc(publication):
+def bbc(publication="bbc"):
     if publication:
-        return get_news(publication)
+        #return get_news(publication)
+        #return get_news1(publication)
+        return get_news2(publication)
+
     else:
         return "this is test"
 
 def get_news(publication):
     feed=feedparser.parse(RSS_FEEDS[publication])
     first_article=feed['entries'][0]
-    return """ <html>
-    <body>
-    <h1> Headerlines</h1>
-    <b>{0}</b><br>
-    <i>{1}</i><br>
-    <p>{2}</p><br>
-    
-    </body>
-    </html>""".format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
+    return render_template("home.html",
+        title=first_article.get("title"),
+        published=first_article.get("published"),
+        summary=first_article.get("summary"))
+def get_news1(publication):
+    feed=feedparser.parse(RSS_FEEDS[publication])
+    first_article=feed['entries'][0]
+    return render_template("home1.html", article=first_article)
+
+def get_news2(publication):
+    feed=feedparser.parse(RSS_FEEDS[publication])
+    articles=feed['entries']
+    return render_template("home2.html", articles=articles)
+
 
 if __name__=='__main__':
     app.run(port=5000,debug=True)
